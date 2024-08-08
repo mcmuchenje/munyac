ad774551f12ad5fdbb232c698c7e3caa98834dd3
Munyaradzi Muchenje <muchenje.munyaradzi@gmail.com>
2024-08-08 19:46:54 +0200

    added changes

---
+++ build.js
@@ -0,0 +1,10 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+  entryPoints: ['./functions/api/submit.js'],  // Path to your Worker entry file
+  bundle: true,
+  minify: true,
+  target: 'es2020',
+  outfile: './public/worker.js',  // Output file for the bundled code
+  platform: 'browser',  // Important for Cloudflare Workers
+}).catch(() => process.exit(1));
