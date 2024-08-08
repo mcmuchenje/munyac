a2093de60bba9f3146e1067d84fcef2c49b3a201
Munyaradzi Muchenje <muchenje.munyaradzi@gmail.com>
2024-08-08 19:12:01 +0200

    added changes

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,6 +1,11 @@
 /**
  * POST /api/submit
  */
+
+import { EmailMessage } from "cloudflare:email";
+import { createMimeMessage } from "mimetext";
+
+
 export async function onRequestPost(context) {
     try {
       let input = await context.request.formData();
@@ -18,6 +23,28 @@ export async function onRequestPost(context) {
       }
   
       let pretty = JSON.stringify(output, null, 2);
+
+      const msg = createMimeMessage();
+   msg.setSender({ name: "GPT-4", addr: "sequal@tiny.africa" });
+   msg.setRecipient("muchenje.munyaradzi@gmail.com");
+   msg.setSubject("An email generated in a worker");
+   msg.addMessage({
+       contentType: 'text/plain',
+       data: `${pretty.movies} Congratulations, you just sent an email from a worker.`
+   });
+
+   var message = new EmailMessage(
+     "sequal@tiny.africa",
+     "muchenje.munyaradzi@gmail.com",
+     msg.asRaw()
+   );
+   try {
+     await env.SEB.send(message);
+   } catch (e) {
+     return new Response(e.message);
+   }
+
+
       return new Response(pretty, {
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
