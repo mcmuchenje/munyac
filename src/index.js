8aa65e25d6e0840cf1fc4c89fba98230417f3384
Munyaradzi Muchenje <muchenje.munyaradzi@gmail.com>
2024-08-08 20:09:00 +0200

    added changes

--- src/index.js
+++ src/index.js
@@ -6,51 +6,51 @@ import { EmailMessage } from "cloudflare:email";
 import { createMimeMessage } from "mimetext";
 
 
-export async function onRequestPost(context) {
-    try {
-      let input = await context.request.formData();
-  
-      // Convert FormData to JSON
-      // NOTE: Allows multiple values per key
-      let output = {};
-      for (let [key, value] of input) {
-        let tmp = output[key];
-        if (tmp === undefined) {
-          output[key] = value;
-        } else {
-          output[key] = [].concat(tmp, value);
-        }
+export default {
+	async fetch(req) {
+		if (req.method === 'POST') {
+		try {
+			const formData = await req.formData();
+        const name = formData.get('name'); // Extract the form field 'name'
+        const greeting = greetUser(name);  // Call your function with the extracted data
+
+        return new Response(greeting, {
+          headers: { 'Content-Type': 'text/plain' },
+        });
+      } catch (error) {
+        return new Response('Error processing request', { status: 500 });
       }
-  
-      let pretty = JSON.stringify(output, null, 2);
-
-      const msg = createMimeMessage();
-   msg.setSender({ name: "GPT-4", addr: "sequal@tiny.africa" });
-   msg.setRecipient("muchenje.munyaradzi@gmail.com");
-   msg.setSubject("An email generated in a worker");
-   msg.addMessage({
-       contentType: 'text/plain',
-       data: `${pretty.movies} Congratulations, you just sent an email from a worker.`
-   });
-
-   var message = new EmailMessage(
-     "sequal@tiny.africa",
-     "muchenje.munyaradzi@gmail.com",
-     msg.asRaw()
-   );
-   try {
-     await env.SEB.send(message);
-   } catch (e) {
-     return new Response(e.message);
-   }
-
-
-      return new Response(pretty, {
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-        },
-      });
-    } catch (err) {
-      return new Response('Error parsing JSON content', { status: 400 });
-    }
+	  
+		// 	const msg = createMimeMessage();
+		//  msg.setSender({ name: "GPT-4", addr: "sequal@tiny.africa" });
+		//  msg.setRecipient("muchenje.munyaradzi@gmail.com");
+		//  msg.setSubject("An email generated in a worker");
+		//  msg.addMessage({
+		// 	 contentType: 'text/plain',
+		// 	 data: `${pretty.movies} Congratulations, you just sent an email from a worker.`
+		//  });
+	  
+		//  var message = new EmailMessage(
+		//    "sequal@tiny.africa",
+		//    "muchenje.munyaradzi@gmail.com",
+		//    msg.asRaw()
+		//  );
+		//  try {
+		//    await env.SEB.send(message);
+		//  } catch (e) {
+		//    return new Response(e.message);
+		//  }
+	  
+	  
+		// 	return new Response(pretty, {
+		// 	  headers: {
+		// 		'Content-Type': 'application/json;charset=utf-8',
+		// 	  },
+		// 	});
+		//   } catch (err) {
+		// 	return new Response('Error parsing JSON content', { status: 400 });
+		//   }
+	}
+}
+    
   }
